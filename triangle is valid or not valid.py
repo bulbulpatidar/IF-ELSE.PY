075547ca0ba9418b2dd1b1e60cbcccce0aa68f57
bulbulpatidar <105332995+bulbulpatidar@users.noreply.github.com>
2022-05-11 08:29:17 +0530

    Add files via upload

---
+++ triangle is valid or not valid.py
@@ -0,0 +1,8 @@
+triangle1=int(input("enter the number"))
+triangle2=int(input("enter the number"))
+triangle3=int(input("enter the number"))
+total=triangle1+triangle2+triangle3
+if total==180:
+    print("triangle is valid ")
+else:
+    print("triangle is not valid")    
