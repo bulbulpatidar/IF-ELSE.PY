a16fbd6312225a735626b2e316860bd33856f75b
bulbulpatidar <105332995+bulbulpatidar@users.noreply.github.com>
2022-05-13 13:41:16 +0530

    Add files via upload

---
+++ doc que24.py
@@ -0,0 +1,15 @@
+# age1=int(input("enter the age1:-"))
+# age2=int(input("enter the age2:-"))
+# age3=int(input("enter the age3:-"))
+# if age1>age2 and age1>age3:
+#     print("age1 is oldest",age1)
+# elif age2>age1 and age2>age3:
+#     print("age2 is oldest",age2)   
+# elif age3>age1 and age3>age2:
+#     print("age3 is oldest",age3)
+# if age1<age3 and age1<age2:
+#     print("age1 is youngest",age1)
+# elif age2<age1 and age2<age3:
+#     print("age2 is youngest",age2) 
+# else:
+#     print("age3 is youngest",age3)           
