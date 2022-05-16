ff3b57d34efcad2cb483f728455936ec245f5bc8
bulbulpatidar <105332995+bulbulpatidar@users.noreply.github.com>
2022-05-16 16:20:46 +0530

    Add files via upload

---
+++ calculate percentage and grade.py
@@ -0,0 +1,24 @@
+# physics=int(input("enter the number:-"))
+# chemstry=int(input("enter the number:-"))
+# biology=int(input("enter the number:-"))
+# maths=int(input("enter the number:-"))
+# computer=int(input("enter the number:-"))
+# total=500
+# obtain=physics+chemstry+biology+maths+computer
+# percentage=((obtain/total)*100)
+# print("percentage",percentage)
+# if percentage>=90:
+#     print("grade A")
+# elif percentage>=80:
+#     print("grade B")
+# elif percentage>=70:
+#     print("grade C") 
+# elif percentage>=60:
+#     print("grade D")  
+# elif percentage>=40:
+#     print("grade E") 
+# elif percentage<40:
+#     print("grade F") 
+# else:
+#     print("fail")               
+
