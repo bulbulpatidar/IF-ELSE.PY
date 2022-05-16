e3be5505d810efa02c63a5f1d5c35382730b766b
bulbulpatidar <105332995+bulbulpatidar@users.noreply.github.com>
2022-05-16 16:22:09 +0530

    Add files via upload

---
+++ check chrater is lower or uppar.py
@@ -0,0 +1,7 @@
+# a=input("enter the charater")
+# if a>="a"and a<="z":
+#     print("it is lower case letter")
+# elif a>="A" and a<="Z":
+#     print("it is uppar case letter") 
+# else:
+#     print("mixed letter")
---
+++ check vowel or consonent.py
@@ -0,0 +1,8 @@
+from re import A
+
+
+alpha=input("enter the charater")
+if alpha=="a"or alpha=="e" or alpha=="o" or alpha=="i" or alpha=="u":
+    print("it is vowel")
+else:
+    print("it is consonent")    
